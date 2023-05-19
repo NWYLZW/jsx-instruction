aa9beff2bb3b57b67fbcecdf72479d3e8080c8c8
yijie4188 <yijie4188@gmail.com>
2023-05-19 15:49:16 +0800

    feat: make Instructor calc as single

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,15 @@ export interface InstructionMap {
   }) => void | Promise<void>
 }
 
+export type InstructorCalc<T> =
+  keyof InstructionMap extends (infer I extends keyof InstructionMap)
+  ? keyof PickWithType<T, InstructionMap[I]> extends (infer K extends keyof T)
+    ? {
+      [KK in K as `${KK & string}:${I & string}`]: T[KK]
+    }
+    : {}
+  : {}
+
 export type Instructor<T> =
   & T
-  & (
-    keyof InstructionMap extends (infer I extends keyof InstructionMap)
-      ? keyof PickWithType<T, InstructionMap[I]> extends (infer K extends keyof T)
-        ? {
-          [KK in K as `${KK & string}:${I & string}`]: T[KK]
-        }
-        : {}
-      : {}
-  )
+  & InstructorCalc<T>
