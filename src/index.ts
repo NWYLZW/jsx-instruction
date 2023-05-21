21f38d2e87958d12fa84b5663a92a54c92a10820
yijie4188 <yijie4188@gmail.com>
2023-05-21 22:29:42 +0800

    feat: define Instruction interface and refactor InstructorCalc type function

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,54 @@
-import type { PickWithType } from './type'
+import type { PickWithType, U2I } from './type'
 
-export interface InstructionMap {
-  stop: (event: {
-    stopPropagation(): void
-  }) => void | Promise<void>
+export interface InstructionItf {}
+
+export interface InstructionDesc<
+  IsPrev extends boolean = false,
+  CT = never,
+  Exclude extends string = never,
+> {
+  /**
+   * @default false
+   */
+  isPrev?: IsPrev
+  exclude?: Exclude[]
+  convert?: CT
 }
 
-export type InstructorCalc<T> =
-  keyof InstructionMap extends (infer I extends keyof InstructionMap)
-  ? keyof PickWithType<T, InstructionMap[I]> extends (infer K extends keyof T)
-    ? {
-      [KK in K as `${KK & string}:${I & string}`]: T[KK]
-    }
+export type Instruction<
+  T,
+  IsPrev extends boolean = false,
+  CT = never,
+  Exclude extends string = never,
+> =
+  & T
+  & InstructionItf
+  & InstructionDesc<IsPrev, CT, Exclude>
+
+export interface InstructionMap {
+}
+
+export type InstructorCalc<T> = U2I<
+  keyof InstructionMap extends (infer IKey extends keyof InstructionMap)
+    ? IKey extends IKey
+    ? InstructionMap[IKey] extends infer I
+      ? I extends Instruction<infer IT, infer IsPrev, infer ICT, infer E>
+        ? Exclude<keyof PickWithType<T, IT>, E> extends (infer K extends keyof T)
+          ? {
+            [KK in K
+              as [IsPrev] extends [true]
+                ? `${IKey & string}:${KK & string}`
+                : `${KK & string}:${IKey & string}`
+            ]: [ICT] extends [never]
+              ? T[KK]
+              : ICT
+          }
+          : {}
+        : {}
+      : {}
     : {}
-  : {}
+    : {}
+>
 
 export type Instructor<T> =
   & T
