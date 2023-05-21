f2b8ac7b25022fcfad5976267f1d1aa6be64d139
yijie4188 <yijie4188@gmail.com>
2023-05-22 01:11:59 +0800

    feat(demos/react): display custom Component

--- demos/react/src/App.tsx
+++ demos/react/src/App.tsx
@@ -1,3 +1,17 @@
+import 'jsx-instruction/common'
+
+import type { Instructor } from 'jsx-instruction'
+import type { MouseEventHandler } from 'react'
+
+function FooButton(props: Instructor<{
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  onDoubleClick?: MouseEventHandler<HTMLButtonElement>
+}>) {
+  return <button {...props}>
+    foo
+  </button>
+}
+
 export default function App() {
   return <div onClick={(e) => {
     console.log('click', e.target)
@@ -12,5 +26,8 @@ export default function App() {
     }}>
       click with stop
     </button>
+    <FooButton onClick:stop={() => {
+      console.log('click with stop')
+    }} />
   </div>
 }
