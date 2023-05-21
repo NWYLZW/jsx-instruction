9efdaa8437fb68ff3e198f831e20a134e0dd4a91
yijie4188 <yijie4188@gmail.com>
2023-05-21 22:36:33 +0800

    refactor: abstract InstructionFieldDefine type function

--- src/index.ts
+++ src/index.ts
@@ -28,21 +28,28 @@ export type Instruction<
 export interface InstructionMap {
 }
 
+export type InstructionFieldDefine<
+  T, CT,
+  K extends keyof T,
+  IKey,
+  IsPrev,
+> = {
+  [KK in K
+    as [IsPrev] extends [true]
+    ? `${IKey & string}:${KK & string}`
+    : `${KK & string}:${IKey & string}`
+  ]: [CT] extends [never]
+    ? T[KK]
+    : CT
+}
+
 export type InstructorCalc<T> = U2I<
   keyof InstructionMap extends (infer IKey extends keyof InstructionMap)
     ? IKey extends IKey
     ? InstructionMap[IKey] extends infer I
       ? I extends Instruction<infer IT, infer IsPrev, infer ICT, infer E>
         ? Exclude<keyof PickWithType<T, IT>, E> extends (infer K extends keyof T)
-          ? {
-            [KK in K
-              as [IsPrev] extends [true]
-                ? `${IKey & string}:${KK & string}`
-                : `${KK & string}:${IKey & string}`
-            ]: [ICT] extends [never]
-              ? T[KK]
-              : ICT
-          }
+          ? InstructionFieldDefine<T, ICT, K, IKey, IsPrev>
           : {}
         : {}
       : {}
