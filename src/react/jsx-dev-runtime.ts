157bb3bd884ec3757822c3e8094938d978412137
yijie4188 <yijie4188@gmail.com>
2023-05-23 01:04:55 +0800

    feat: props resolve implement

--- src/react/jsx-dev-runtime.ts
+++ src/react/jsx-dev-runtime.ts
@@ -1,5 +1,7 @@
 import * as __Runtime__ from 'react/jsx-dev-runtime'
 
+import { instructors } from '../index'
+
 const Runtime = __Runtime__ as {
   jsxs(type: any, props: any, key: any): any
   jsxDEV(type: any, props: any, key: any, isStaticChildren: any, source: any, self: any): any
@@ -13,16 +15,23 @@ export function jsxs(type: any, props: any, key: any) {
 export function jsxDEV(type: any, props: any, key: any, isStaticChildren: any, source: any, self: any) {
   const propsWrap = {} as Record<string, any>
   for (const key in props) {
-    if (key.endsWith(':stop')) {
-      propsWrap[key.replace(':stop', '')] = (e: MouseEvent) => {
-        e.stopPropagation()
-        props[key](e)
-      }
-    } else {
+    let match = false
+    Object.entries(instructors)
+      .forEach(([name, instruction]) => {
+        if (
+          (instruction.isPrev && key.startsWith(name + ':')) ||
+          (!instruction.isPrev && key.endsWith(':' + name))
+        ) {
+          const originalKey = instruction.isPrev
+            ? key.slice(name.length + 1)
+            : key.slice(0, key.length - name.length - 1)
+          propsWrap[originalKey] = instruction(props[key])
+          match = true
+        }
+      })
+    if (!match)
       propsWrap[key] = props[key]
-    }
   }
-  // console.log('jsxDEV', type, propsWrap, key, isStaticChildren, source, self)
   return Runtime.jsxDEV(type, propsWrap, key, isStaticChildren, source, self)
 }
 
