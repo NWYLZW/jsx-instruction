7058b297da89befca467633ecfe234cab4f16e82
yijie4188 <yijie4188@gmail.com>
2023-05-23 16:01:32 +0800

    feat: all HTMLAttribute support

--- src/react/index.d.ts
+++ src/react/index.d.ts
@@ -3,6 +3,56 @@ import type * as React from 'react'
 import type { InstructorCalc } from '../index'
 
 declare module 'react' {
+  interface HTMLAttributes<T> extends InstructorCalc<React.HTMLAttributes<T>> {}
   interface AnchorHTMLAttributes<T> extends InstructorCalc<React.AnchorHTMLAttributes<T>> {}
+  interface AudioHTMLAttributes<T> extends InstructorCalc<React.AudioHTMLAttributes<T>> {}
+  interface AreaHTMLAttributes<T> extends InstructorCalc<React.AreaHTMLAttributes<T>> {}
+  interface BaseHTMLAttributes<T> extends InstructorCalc<React.BaseHTMLAttributes<T>> {}
+  interface BlockquoteHTMLAttributes<T> extends InstructorCalc<React.BlockquoteHTMLAttributes<T>> {}
   interface ButtonHTMLAttributes<T> extends InstructorCalc<React.ButtonHTMLAttributes<T>> {}
+  interface CanvasHTMLAttributes<T> extends InstructorCalc<React.CanvasHTMLAttributes<T>> {}
+  interface ColHTMLAttributes<T> extends InstructorCalc<React.ColHTMLAttributes<T>> {}
+  interface ColgroupHTMLAttributes<T> extends InstructorCalc<React.ColgroupHTMLAttributes<T>> {}
+  interface DataHTMLAttributes<T> extends InstructorCalc<React.DataHTMLAttributes<T>> {}
+  interface DetailsHTMLAttributes<T> extends InstructorCalc<React.DetailsHTMLAttributes<T>> {}
+  interface DelHTMLAttributes<T> extends InstructorCalc<React.DelHTMLAttributes<T>> {}
+  interface DialogHTMLAttributes<T> extends InstructorCalc<React.DialogHTMLAttributes<T>> {}
+  interface EmbedHTMLAttributes<T> extends InstructorCalc<React.EmbedHTMLAttributes<T>> {}
+  interface FieldsetHTMLAttributes<T> extends InstructorCalc<React.FieldsetHTMLAttributes<T>> {}
+  interface FormHTMLAttributes<T> extends InstructorCalc<React.FormHTMLAttributes<T>> {}
+  interface HtmlHTMLAttributes<T> extends InstructorCalc<React.HtmlHTMLAttributes<T>> {}
+  interface IframeHTMLAttributes<T> extends InstructorCalc<React.IframeHTMLAttributes<T>> {}
+  interface ImgHTMLAttributes<T> extends InstructorCalc<React.ImgHTMLAttributes<T>> {}
+  interface InsHTMLAttributes<T> extends InstructorCalc<React.InsHTMLAttributes<T>> {}
+  interface InputHTMLAttributes<T> extends InstructorCalc<React.InputHTMLAttributes<T>> {}
+  interface KeygenHTMLAttributes<T> extends InstructorCalc<React.KeygenHTMLAttributes<T>> {}
+  interface LabelHTMLAttributes<T> extends InstructorCalc<React.LabelHTMLAttributes<T>> {}
+  interface LiHTMLAttributes<T> extends InstructorCalc<React.LiHTMLAttributes<T>> {}
+  interface LinkHTMLAttributes<T> extends InstructorCalc<React.LinkHTMLAttributes<T>> {}
+  interface MapHTMLAttributes<T> extends InstructorCalc<React.MapHTMLAttributes<T>> {}
+  interface MenuHTMLAttributes<T> extends InstructorCalc<React.MenuHTMLAttributes<T>> {}
+  interface MediaHTMLAttributes<T> extends InstructorCalc<React.MediaHTMLAttributes<T>> {}
+  interface MetaHTMLAttributes<T> extends InstructorCalc<React.MetaHTMLAttributes<T>> {}
+  interface MeterHTMLAttributes<T> extends InstructorCalc<React.MeterHTMLAttributes<T>> {}
+  interface QuoteHTMLAttributes<T> extends InstructorCalc<React.QuoteHTMLAttributes<T>> {}
+  interface ObjectHTMLAttributes<T> extends InstructorCalc<React.ObjectHTMLAttributes<T>> {}
+  interface OlHTMLAttributes<T> extends InstructorCalc<React.OlHTMLAttributes<T>> {}
+  interface OptgroupHTMLAttributes<T> extends InstructorCalc<React.OptgroupHTMLAttributes<T>> {}
+  interface OptionHTMLAttributes<T> extends InstructorCalc<React.OptionHTMLAttributes<T>> {}
+  interface OutputHTMLAttributes<T> extends InstructorCalc<React.OutputHTMLAttributes<T>> {}
+  interface ParamHTMLAttributes<T> extends InstructorCalc<React.ParamHTMLAttributes<T>> {}
+  interface ProgressHTMLAttributes<T> extends InstructorCalc<React.ProgressHTMLAttributes<T>> {}
+  interface SlotHTMLAttributes<T> extends InstructorCalc<React.SlotHTMLAttributes<T>> {}
+  interface ScriptHTMLAttributes<T> extends InstructorCalc<React.ScriptHTMLAttributes<T>> {}
+  interface SelectHTMLAttributes<T> extends InstructorCalc<React.SelectHTMLAttributes<T>> {}
+  interface SourceHTMLAttributes<T> extends InstructorCalc<React.SourceHTMLAttributes<T>> {}
+  interface StyleHTMLAttributes<T> extends InstructorCalc<React.StyleHTMLAttributes<T>> {}
+  interface TableHTMLAttributes<T> extends InstructorCalc<React.TableHTMLAttributes<T>> {}
+  interface TextareaHTMLAttributes<T> extends InstructorCalc<React.TextareaHTMLAttributes<T>> {}
+  interface TdHTMLAttributes<T> extends InstructorCalc<React.TdHTMLAttributes<T>> {}
+  interface ThHTMLAttributes<T> extends InstructorCalc<React.ThHTMLAttributes<T>> {}
+  interface TimeHTMLAttributes<T> extends InstructorCalc<React.TimeHTMLAttributes<T>> {}
+  interface TrackHTMLAttributes<T> extends InstructorCalc<React.TrackHTMLAttributes<T>> {}
+  interface VideoHTMLAttributes<T> extends InstructorCalc<React.VideoHTMLAttributes<T>> {}
+  interface WebViewHTMLAttributes<T> extends InstructorCalc<React.WebViewHTMLAttributes<T>> {}
 }
